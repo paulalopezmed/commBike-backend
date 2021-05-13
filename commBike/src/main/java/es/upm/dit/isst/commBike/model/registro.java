1163a7c5c6bafc27e7566e84bd96009351a90560
paulalopezmed <plm1441@gmail.com>
2021-05-13 17:25:42 +0200

    Sprint3

--- commBike/src/main/java/es/upm/dit/isst/commBike/model/registro.java
+++ commBike/src/main/java/es/upm/dit/isst/commBike/model/registro.java
@@ -14,7 +14,7 @@ public class registro implements Serializable {
 	private static final long serialVersionUID=1L;
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+//	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private String recordCode;
 	private String bikeCode;
 	private double originBikeCoordenatesX;
